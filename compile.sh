12afabc4c015cdd208fdf01a9800769edbafd4b7
hahnec <christopher.hahne@unibe.ch>
2023-08-23 18:24:13 +0200

    feat(png): enable png slide export

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+nvm install v18.17.0
+
 if [ $# == 0 ]; then
     # ask for filename, if no args provided
     echo -n "Please enter a filename: "
@@ -19,3 +21,8 @@ echo ""
 echo "Starting HTML file generation ..."
 npx @marp-team/marp-cli $1 --html --theme-set ./themes/
 echo "HTML file generation completed"
+
+# png file export
+filename="$1"
+pdf_filename="${filename%.*}.pdf"
+convert -density 150 $pdf_filename -resize 96% slide-%03d.png
